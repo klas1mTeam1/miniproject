2476b4aaa6098a63032c7f3b941f9459f8ab28c0
marcelo16bo <marcelo.penalozaotarola@student.hu.nl>
2015-10-26 17:43:45 +0100

    Voorpagina

--- code.py
+++ code.py
@@ -1,2 +1,23 @@
 __author__ = 'marce'
 
+from tkinter import *
+
+root = Tk()
+root.title("Super Wonder Captain")
+
+root.withdraw()
+root.update_idletasks()
+x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 3
+y = (root.winfo_screenheight() - root.winfo_reqheight()) / 3
+root.geometry("+%d+%d" % (x, y))
+root.deiconify()
+
+frontpic = PhotoImage(file="frontpage_pic.gif")
+label = Label(root, image=frontpic)
+label.pack()
+
+start_knop = Button(text="Click here to start the game", fg="blue")
+start_knop.pack(side=BOTTOM)
+
+
+root.mainloop()
