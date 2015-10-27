792fbefd5179fbf9d8617dbb588ca836f6d29232
Youri van Dijk <youri.vandijk@live.nl>
2015-10-27 16:05:38 +0100

    Beginversie voor scherm na het indrukken van de knop op het beginscherm.
    Venster 2 met knoppen. (Knoppen nog niet helemaal werkend)

---
+++ Knop_Code.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import tkinter as tk
+
+window = Tk()
+
+#Zet het venster in het midden van het scherm.
+window.withdraw()
+window.update_idletasks()
+x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2.1
+y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2.1
+window.geometry("+%d+%d" % (x, y))
+window.deiconify()
+
+#Geeft het venster standaard NS geel achtergrond.
+window.tk_setPalette(background='#FECE22')
+
+#Standaard venster met keuze.
+window.title("Actuele vertrektijden")
+label = Label(window, text='Van welk station wilt u de actuele vertrektijden?', fg='#003399')
+label.pack()
+
+#Geeft een tekstvakje en "OK" knop onder de knop "Ander station" als je er op klikt. (Returned de ingevoerde tekst nog niet)
+def tekstvak():
+    tekstvakje = tk.Entry(window, bg='white')
+    tekstvakje.pack(pady=5)
+
+    ok_knop = Button(window, text='OK', fg="white", bg="#003399", activebackgroun = "white", activeforeground = "#003399")
+    ok_knop.pack(pady=5)
+
+#Voegt een knop aan het venster om te kiezen voor het huidige station. (Doet nog niks)
+knop_huidig_station = Button(window, text="Utrecht Centraal", fg="white", bg="#003399", activebackgroun = "white", activeforeground = "#003399")
+knop_huidig_station.pack(pady=5)
+
+#Voegt een knip aan het venster om te kiezen voor een ander station.
+knop_ander_station = Button(window, text="Ander station", fg="white", bg="#003399", activebackgroun = "white", activeforeground = "#003399", command = (tekstvak))
+knop_ander_station.pack(pady=5)
+
+window.mainloop()
