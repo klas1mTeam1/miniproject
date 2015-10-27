46ed27129df54d403590e2969d7dce525c9f926b
Martijn <martijn.dull@student.hu.nl>
2015-10-27 17:14:35 +0100

    type trein toegevoegd

--- Utrecht Centraal API.py
+++ Utrecht Centraal API.py
@@ -4,9 +4,9 @@ import requests
 import codecs
 import xmltodict
 
-auth_details = ('martijn.dull@student.hu.nl', '0yZyZgme8551xHmiqvTNBxl-iMl0xOPZ0pDQxbTN2-R5ZWQQXrvRwA')
+auth_details = ('martijn.dull@student.hu.nl', '0yZyZgme8551xHmiqvTNBxl-iMl0xOPZ0pDQxbTN2-R5ZWQQXrvRwA') #inlogcodes NS-API
 
-actueel_utrecht = requests.get('http://webservices.ns.nl/ns-api-avt?station=ut', auth=auth_details) #vertrektijden Utrecht Centraal
+actueel_utrecht = requests.get('http://webservices.ns.nl/ns-api-avt?station=ut', auth=auth_details) #actuele vertrekinformatie Utrecht Centraal
 
 def schrijf_actueel_utrecht_xml(actueel_utrecht): #schrijft een xml bestand van de actuele vertrekinformatie Station Utrecht Centraal
     bestand = open('actueel_utrecht.xml', 'w')
@@ -16,15 +16,17 @@ def schrijf_actueel_utrecht_xml(actueel_utrecht): #schrijft een xml bestand van 
 
 schrijf_actueel_utrecht_xml(actueel_utrecht)
 
-def verwerk_actueel_utrecht_xml():
+def verwerk_actueel_utrecht_xml(): #verwerkt actuele vertrekinformatie Utrecht Centraal xml naar dictionary
     bestand = open('actueel_utrecht.xml', 'r')
     xml_string = bestand.read()
     return xmltodict.parse(xml_string)
+    bestand.close()
 
 def print_actueel_utrecht(actueel_utrecht_dict): #print de actuele vertrekinformatie van Station Utrecht Centraal
     for rit in actueel_utrecht_dict['ActueleVertrekTijden']['VertrekkendeTrein']:
-        print('De trein van ' + str(rit['VertrekTijd'][11:16]) +
-              ' richting ' + str(rit['EindBestemming'])
+        print('De ' + str(rit['TreinSoort'])
+              + ' van ' + str(rit['VertrekTijd'][11:16])
+              + ' richting ' + str(rit['EindBestemming'])
               + ' vertrekt vanaf spoor ' + str(rit['VertrekSpoor']['#text']) + '.')
 
 actueel_utrecht_dict = verwerk_actueel_utrecht_xml()
