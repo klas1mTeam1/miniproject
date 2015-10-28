506f37905e70731b463030d47988de40e55c6977
Youri van Dijk <youri.vandijk@live.nl>
2015-10-28 15:17:30 +0100

    Update Knop_Code

--- Knop_Code.py
+++ Knop_Code.py
@@ -3,39 +3,51 @@ import tkinter as tk
 
 window = Tk()
 
-#Zet het venster in het midden van het scherm.
+# Instellingen voor venster grootte en positie.
 window.withdraw()
 window.update_idletasks()
-w = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2.1
-h = (window.winfo_screenheight() - window.winfo_reqheight()) / 2.1
-window.geometry("+%d+%d" % (w, h))
+w = 700 # Breedte van het venster.
+h = 500 # Hoogte van het venster.
+
+ws = window.winfo_screenwidth() # Breedte van het scherm.
+hs = window.winfo_screenheight() # Hoogte van het scherm.
+
+# x en y coordinaten berekenen van het venster.
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# Zet het venster op de goede plek met de goede grootte.
+window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
 window.deiconify()
 
-#Geeft het venster standaard NS geel achtergrond.
+# Geeft het venster standaard NS geel achtergrond.
 window.tk_setPalette(background='#FECE22')
 
-#Standaard venster met keuze.
+# Standaard venster met keuze.
 window.title("Actuele vertrektijden")
-label = Label(window, text='Van welk station wilt u de actuele vertrektijden opvragen?', fg='#003399')
-label.pack()
+titel = Label(window, text='Actuele vertrektijden.\n', fg='#003399', font = ('Ariel',24, 'bold'))
+titel.pack()
+subtitel = Label(window, text='Van welk station wilt u de actuele vertrektijden opvragen?\n', fg='#003399', font = ('Ariel',18, 'bold'))
+subtitel.pack()
 
-#Geeft een tekstvakje en "OK" knop onder de knop "Ander station" als je er op klikt. (Returned de ingevoerde tekst nog niet)
+# Geeft een tekstvakje en "OK" knop onder de knop "Ander station" als je er op klikt. (Returned de ingevoerde tekst nog niet)
 def tekstvak():
     tekstvakje = tk.Entry(window, bg='white')
     tekstvakje.pack(pady=5)
     tekstvakje.focus_set()
-    #Zorgt voor het returnen van de ingevulde tekst.
+    # Zorgt voor het returnen van de ingevulde tekst.
     def callback():
         content = tekstvakje.get()
         print(content)
     ok_knop = Button(window, text='OK', fg="white", bg="#003399", activebackground = "white", activeforeground = "#003399", command = callback)
     ok_knop.pack(pady=5)
 
-#Voegt een knop aan het venster om te kiezen voor het huidige station. (Doet nog niks)
+# Voegt een knop aan het venster om te kiezen voor het huidige station. (Doet nog niks)
 knop_huidig_station = Button(window, text="Utrecht\nCentraal", fg="white", bg="#003399", activebackgroun = "white", activeforeground = "#003399", height = 2, width = 10)
 knop_huidig_station.pack(pady=5)
 
-#Voegt een knip aan het venster om te kiezen voor een ander station.
+# Voegt een knip aan het venster om te kiezen voor een ander station.
 knop_ander_station = Button(window, text="Ander\nstation", fg="white", bg="#003399", activebackgroun = "white", activeforeground = "#003399", height = 2, width = 10, command = (tekstvak))
 knop_ander_station.pack(pady=5)
 
