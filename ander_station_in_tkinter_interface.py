2a146d2ce64839fc0dc71aa57b4407f2dd7fe8c1
Youri van Dijk <youri.vandijk@live.nl>
2015-10-30 11:23:56 +0100

    Ander station code in één functie gezet

--- ander_station_in_tkinter_interface.py
+++ ander_station_in_tkinter_interface.py
@@ -8,90 +8,91 @@ import sys
 import Startscherm
 import Knop_Code
 
-def terug_hoofdmenu():
-    window.destroy()
-    Startscherm.create_window()
-
-def terug():
-    window.destroy()
-    Knop_Code.scherm()
-
-def get_input():
-    global content
-    bestand = open('check_station.txt', 'r')
-    content = bestand.read()
-    bestand.close()
-
-get_input()
-
-auth_details = ('martijn.dull@student.hu.nl', '0yZyZgme8551xHmiqvTNBxl-iMl0xOPZ0pDQxbTN2-R5ZWQQXrvRwA') #inlogcodes NS-API
-
-try:
-    url = 'http://webservices.ns.nl/ns-api-avt?station='
-    as_id = content
-    new_url = "{}{}".format(url,as_id)
-    actueel_as = requests.get(new_url, auth=auth_details) #actuele vertrekinformatie van de gewenste station
-except:
-    print("Kan geen verbinding maken met de NS API.")
-    sys.exit()
-
-def schrijf_actueel_as_xml(actueel_as): #schrijft een xml bestand van de actuele vertrekinformatie station
-    bestand = open('actueel_as.xml', 'w')
-    bestand = codecs.open('actueel_as.xml', "w", "utf-8")
-    bestand.write(str(actueel_as.text))
-    bestand.close()
-
-schrijf_actueel_as_xml(actueel_as)
-
-def verwerk_actueel_as_xml(): #verwerkt actuele vertrekinformatie station xml naar dictionary
-    bestand = open('actueel_as.xml', 'r')
-    xml_string = bestand.read()
-    bestand.close()
-    return xmltodict.parse(xml_string)
-
-def plaats_actueel_as_op_grid(root, actueel_as_dict): #print de actuele vertrekinformatie van station
-    index = 0
-
-    Label(topframe, text='Tijd', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=0, sticky=NW)
-    Label(topframe, text='Naar', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=1, sticky=NW)
-    Label(topframe, text='Spoor', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=2, sticky=NW)
-    Label(topframe, text='Via', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=3, sticky=NW)
-    Label(topframe, text='Reisdetails', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=4, sticky=NW)
-
-    result = ""
-    for rit in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein']:
-        if index==18:
-            break
-
-        if 'RouteTekst' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
-            routetekst  =  str(rit['RouteTekst'])
-        else:
-            routetekst   = ""
-
-        if 'Opmerkingen' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
-            opmerkingen = ', ' + (str(rit['Opmerkingen']['Opmerking']))
-        else:
-            opmerkingen  = ""
-
-        if 'VertrekVertragingTekst' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
-            VertekVertragingTekst = (str(rit['VertrekVertragingTekst']))
-        else:
-            VertekVertragingTekst = ""
-
-        Label(topframe, text=str(rit['VertrekTijd'][11:16]) + ' ' + str(VertekVertragingTekst), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=0, sticky=NW)
-        Label(topframe, text=str(rit['EindBestemming']), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=1, sticky=NW)
-        Label(topframe, text=str(rit['VertrekSpoor']['#text']), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=2, sticky=NW)
-        Label(topframe, text=str(routetekst), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=3, sticky=NW)
-        Label(topframe, text=str(rit['TreinSoort']) + (opmerkingen), wraplength = 100, justify = LEFT, fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=4, sticky=NW)
-
-        index += 1
-
-    return result
-
-actueel_as_dict = verwerk_actueel_as_xml()
-
-
 def as_scherm():
+    def terug_hoofdmenu():
+        window.destroy()
+        Startscherm.create_window()
+
+    def terug():
+        window.destroy()
+        Knop_Code.scherm()
+
+    def get_input():
+        global content
+        bestand = open('check_station.txt', 'r')
+        content = bestand.read()
+        bestand.close()
+
+    get_input()
+
+    auth_details = ('martijn.dull@student.hu.nl', '0yZyZgme8551xHmiqvTNBxl-iMl0xOPZ0pDQxbTN2-R5ZWQQXrvRwA') #inlogcodes NS-API
+
+    try:
+        url = 'http://webservices.ns.nl/ns-api-avt?station='
+        as_id = content
+        new_url = "{}{}".format(url,as_id)
+        actueel_as = requests.get(new_url, auth=auth_details) #actuele vertrekinformatie van de gewenste station
+    except:
+        print("Kan geen verbinding maken met de NS API.")
+        sys.exit()
+
+    def schrijf_actueel_as_xml(actueel_as): #schrijft een xml bestand van de actuele vertrekinformatie station
+        bestand = open('actueel_as.xml', 'w')
+        bestand = codecs.open('actueel_as.xml', "w", "utf-8")
+        bestand.write(str(actueel_as.text))
+        bestand.close()
+
+    schrijf_actueel_as_xml(actueel_as)
+
+    def verwerk_actueel_as_xml(): #verwerkt actuele vertrekinformatie station xml naar dictionary
+        bestand = open('actueel_as.xml', 'r')
+        xml_string = bestand.read()
+        bestand.close()
+        return xmltodict.parse(xml_string)
+
+    def plaats_actueel_as_op_grid(root, actueel_as_dict): #print de actuele vertrekinformatie van station
+        index = 0
+
+        Label(topframe, text='Tijd', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=0, sticky=NW)
+        Label(topframe, text='Naar', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=1, sticky=NW)
+        Label(topframe, text='Spoor', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=2, sticky=NW)
+        Label(topframe, text='Via', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=3, sticky=NW)
+        Label(topframe, text='Reisdetails', fg='#003399', font = ('Ariel',9, 'bold')).grid(row=0,column=4, sticky=NW)
+
+        result = ""
+        for rit in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein']:
+            if index > 16:
+                break
+
+            if 'RouteTekst' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
+                routetekst  =  str(rit['RouteTekst'])
+            else:
+                routetekst   = ""
+
+            if 'Opmerkingen' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
+                opmerkingen = ', ' + (str(rit['Opmerkingen']['Opmerking']))
+            else:
+                opmerkingen  = ""
+
+            if 'VertrekVertragingTekst' in actueel_as_dict['ActueleVertrekTijden']['VertrekkendeTrein'][index]:
+                VertekVertragingTekst = (str(rit['VertrekVertragingTekst']))
+            else:
+                VertekVertragingTekst = ""
+
+            Label(topframe, text=str(rit['VertrekTijd'][11:16]) + ' ' + str(VertekVertragingTekst), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=0, sticky=NW)
+            Label(topframe, text=str(rit['EindBestemming']), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=1, sticky=NW)
+            Label(topframe, text=str(rit['VertrekSpoor']['#text']), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=2, sticky=NW)
+            Label(topframe, text=str(routetekst), fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=3, sticky=NW)
+            Label(topframe, text=str(rit['TreinSoort']) + (opmerkingen), wraplength = 100, justify = LEFT, fg='#003399', font = ('Ariel',9, 'bold')).grid(row=index+1,column=4, sticky=NW)
+
+            index += 1
+
+        return result
+
+    actueel_as_dict = verwerk_actueel_as_xml()
+
+
+
     global window
     window = Tk()
 
